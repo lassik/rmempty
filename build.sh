e1b3517305f0344731780bd120cb344926572fd8
Lassi Kortela <lassi@lassi.io>
2019-10-24 18:54:06 +0300

    Fix quoting

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
-export CC=${CC:-gcc}
-export CFLAGS=${CFLAGS:--Wall -Wextra -Werror -std=gnu99 -Og -g}
+export CC="${CC:-gcc}"
+export CFLAGS="${CFLAGS:--Wall -Wextra -Werror -std=gnu99 -Og -g}"
 b="build-$(uname | tr A-Z- a-z_)-$(uname -m | tr A-Z- a-z_)-$CC"
 mkdir -p -- "$b"
 cd "$(dirname "$0")/$b"

11722995f3ae2ef34c8dd955222b8f12d924d090
Lassi Kortela <lassi@lassi.io>
2019-10-24 19:07:48 +0300

    Fix mkdir in build script

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ set -eu
 export CC="${CC:-gcc}"
 export CFLAGS="${CFLAGS:--Wall -Wextra -Werror -std=gnu99 -Og -g}"
 b="build-$(uname | tr A-Z- a-z_)-$(uname -m | tr A-Z- a-z_)-$CC"
+cd "$(dirname "$0")"
 mkdir -p -- "$b"
-cd "$(dirname "$0")/$b"
+cd "$b"
 echo "Entering directory '$PWD'"
 $CC $CFLAGS -o rmempty ../rmempty.c \
     -D "PROGVERSION=\"$(git describe --always --dirty)\""
